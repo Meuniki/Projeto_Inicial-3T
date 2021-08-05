5e22bfa2c28b688ba423dcaf07bc533abc38cac5
Meuniki <castro.rocha.rcr@gmail.com>
2021-08-04 23:44:50 -0300

    CRUD de salas e equipamentos

---
+++ Back-End/Back-End/Porject_scool/Porject_scool/Controllers/EquipamentosController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Porject_scool.Domains;
+using Porject_scool.Interfaces;
+using Porject_scool.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Porject_scool.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EquipamentosController : ControllerBase
+    {
+        /// <summary>
+        /// Objeto _equipamentoRepository que irá receber todos métodos definidos na interface IEquipamentoRepository
+        /// </summary>
+        private IEquipamentoRepository _equipamentoRepository { get; set; }
+
+        /// <summary>
+        /// Instancia o objeto _equipamentoRepository para que haja a referência aos métodos no repositório
+        /// </summary>
+        public EquipamentosController()
+        {
+            _equipamentoRepository = new EquipamentoRepository();
+        }
+
+        /// <summary>
+        /// Lista todos os equipamentos
+        /// </summary>
+        /// <returns>Uma lista de equipamentos e um status code 200 - Ok</returns>
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_equipamentoRepository.ListarTodos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Busca um equipamento através do seu ID
+        /// </summary>
+        /// <param name="id">ID do equipamento a ser buscado</param>
+        /// <returns>Um equipamento encontrado e um status code 200 - Ok</returns>
+        [Authorize]
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            try
+            {
+                return Ok(_equipamentoRepository.BuscarPorId(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Cadastra um novo equipamento
+        /// </summary>
+        /// <param name="novaEquipamento">Objeto novaEquipamento que será cadastrada</param>
+        /// <returns>Um status 201 - Created</returns>
+        [Authorize]
+        [HttpPost]
+        public IActionResult Post(Equipamento novaEquipamento)
+        {
+            _equipamentoRepository.Cadastrar(novaEquipamento);
+
+            return StatusCode(201);
+        }
+
+        /// <summary>
+        /// Modifica um equipamento existnte
+        /// </summary>
+        /// <param name="id">ID do equipamento que será alterado</param>
+        /// <param name="equipamentoAtualizada">Objeto equipamentoAtualizada com as novas informações</param>
+        /// <returns>Um status code 204 - No Content</returns>
+        [Authorize]
+        [HttpPut]
+        public IActionResult Put(int id, Equipamento equipamentoAtualizada)
+        {
+            _equipamentoRepository.Atualizar(id, equipamentoAtualizada);
+
+            return StatusCode(204);
+        }
+
+        /// <summary>
+        /// Deleta um equipamento existente
+        /// </summary>
+        /// <param name="id">ID do equipamento que será deletado</param>
+        /// <returns>Um status code 204 - No Content</returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _equipamentoRepository.Deletar(id);
+
+            return StatusCode(204);
+        }
+    }
+}
---
+++ Back-End/Back-End/Porject_scool/Porject_scool/Controllers/SalasController.cs
@@ -0,0 +1,133 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Porject_scool.Domains;
+using Porject_scool.Interfaces;
+using Porject_scool.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Porject_scool.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SalasController : ControllerBase
+    {
+        /// <summary>
+        /// Objeto _salaRepository que irá receber todos métodos definidos na interface ISalaRepository
+        /// </summary>
+        private ISalaRepository _salaRepository { get; set; }
+
+        /// <summary>
+        /// Instancia o objeto _salaRepository para que haja a referência aos métodos no repositório
+        /// </summary>
+        public SalasController()
+        {
+            _salaRepository = new SalaRepository();
+        }
+
+        /// <summary>
+        /// Lista todas as salas
+        /// </summary>
+        /// <returns>Uma lista de salas e um status code 200 - Ok</returns>
+        [Authorize]
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(_salaRepository.ListarTodos());
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Busca uma sala através do seu ID
+        /// </summary>
+        /// <param name="id">ID da sala a ser buscada</param>
+        /// <returns>Uma sala encontrada e um status code 200 - Ok</returns>
+        [Authorize]
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        { 
+            try
+            {
+                return Ok(_salaRepository.BuscarPorId(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Cadastra uma nova sala
+        /// </summary>
+        /// <param name="novaSala">Objeto novaSala que será cadastrada</param>
+        /// <returns>Um status code 201 - Created</returns>
+        [Authorize]
+        [HttpPost]
+        public IActionResult Post(Sala novaSala)
+        {
+            try
+            {
+                _salaRepository.Cadastrar(novaSala);
+
+                return StatusCode(201);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Modifica uma sala existnte
+        /// </summary>
+        /// <param name="id">ID da sala que será alterada</param>
+        /// <param name="salaAtualizada">Objeto salaAtualizada com as novas informações</param>
+        /// <returns>Um status code 204 - No Content</returns>
+        [Authorize]
+        [HttpPut]
+        public IActionResult Put(int id, Sala salaAtualizada)
+        {
+            try
+            {
+                _salaRepository.Atualizar(id, salaAtualizada);
+
+                return StatusCode(204);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+        }
+
+        /// <summary>
+        /// Deleta uma sala através do seu ID
+        /// </summary>
+        /// <param name="id">ID da sala a ser deletada</param>
+        /// <returns>Um status code 204 - No Content</returns>
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            try
+            {
+                _salaRepository.Deletar(id);
+
+                return StatusCode(204);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex);
+            }
+
+        }
+    }
+}
