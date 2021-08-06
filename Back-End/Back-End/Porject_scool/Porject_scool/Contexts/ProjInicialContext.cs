dba72db15b4701def253d07f4d977b564b65d55c
Pedro-B-Alves <pedro22carro@gmail.com>
2021-08-06 09:05:46 -0300

    atualizando postman

--- Back-End/Back-End/Porject_scool/Porject_scool/Contexts/ProjInicialContext.cs
+++ Back-End/Back-End/Porject_scool/Porject_scool/Contexts/ProjInicialContext.cs
@@ -28,7 +28,7 @@ namespace Porject_scool.Contexts
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=DESKTOP-RSQ1KON; Initial Catalog= Projeto_Inicial; Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=LAPTOP-AGDJFBQ9\\SQLEXPRESS; Initial Catalog= Projeto_Inicial; Integrated Security=True");
             }
         }
 
