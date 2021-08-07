e7e3d1cec75f0db71c2250e6c92290847e66c03a
Pedro-B-Alves <pedro22carro@gmail.com>
2021-08-07 15:41:30 -0300

    Tela de Cadastro de Equipamentos

--- Projeto School/project/src/index.js
+++ Projeto School/project/src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import CadastroEquipamentos from './pages/CadastroEquipamentos';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const routing = (
+  <Router>
+    <div>
+        <Switch>
+          <Route path="/" component={CadastroEquipamentos}/>
+        </Switch>
+    </div>
+  </Router>
 );
+
+ReactDOM.render( routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ Projeto School/project/src/pages/CadastroEquipamentos.js
@@ -0,0 +1,99 @@
+import "../assets/CSS/CadastroEquipamentos.css";
+import imgUsuario from "../assets/Imagens/Vector.png"
+import livros from "../assets/Imagens/Livros.png"
+import linha from "../assets/Imagens/Vector 45.png"
+import linha2 from "../assets/Imagens/Line 1.png"
+import React, { useState } from 'react';
+import axios from 'axios';
+
+export default function CadastroEquipamentos() {
+
+    const [ marca, setMarca ] = useState( '' );
+    const [ tipoEquipamento, setTipoEquipamento ] = useState( '' );
+    const [ numSerie, setNumSerie ] = useState( '' );
+    const [ ativo , setAtivo ] = useState( false );
+    const [ numPatrimonio , setNumPatrimonio ] = useState( 0 );
+    const [ descricao, setDescricao ] = useState( '' );
+    const [ isLoading, setIsLoading ] = useState( false );
+
+    function cadastrarEquipamento(event){
+        event.preventDefault();
+
+        setIsLoading(true);
+
+        axios.post('http://localhost:5000/api/Equipamentos',{
+            marca : marca,
+            tipoEquipamento : tipoEquipamento,
+            numSerie : numSerie,
+            descricao : descricao,
+            numPatrimonio : numPatrimonio,
+            ativo : ativo
+        })
+        .then(resposta => {
+            if (resposta.status === 201) {
+                console.log('Equipamento Cadastrado!');
+                setIsLoading( false );
+            }
+        })
+
+        .catch(erro => console.log(erro));
+    };
+
+    return(
+        <div className="pagina">
+            <section>
+                <div className="parteSuperior">
+                    <div className="usuario">
+                        <img src={linha2} alt="Linha"/>
+                        <img className="imgUsuario" src={imgUsuario} alt="Foto Do Usuário"/>
+                        <p className="nomeUsuario">Pedro - Adm</p>
+                    </div>
+                </div>
+                <div className="parteInferior">
+                    <img className="livros" src={livros} alt="Livros"/>
+                    <img src={linha} alt="Linha"/>
+                    <form className="blocoCadastro" onSubmit={cadastrarEquipamento}>
+                        <p className="titulo">Cadastro de equipamentos</p>
+                        <div>
+                            <div className="bloco">
+                                <p className="textoCadastro">Marca:</p>
+                                <input className="inputCadastro" type="text" value={marca} onChange={(event) => setMarca(event.target.value)}></input>
+                            </div>
+                            <div className="bloco">
+                                <p className="textoCadastro">Tipo de equipamento:</p>
+                                <input className="inputCadastro" type="text" value={tipoEquipamento} onChange={(event) => setTipoEquipamento(event.target.value)}></input>
+                            </div>
+                            <div className="bloco">
+                                <p className="textoCadastro">Numero de série:</p>
+                                <input className="inputCadastro" type="text" value={numSerie} onChange={(event) => setNumSerie(event.target.value)}></input>
+                            </div>
+                            <div className="bloco">
+                                <p className="textoCadastro">Ativo/Inativo:</p>
+                                <input className="inputCadastro" type="boolean" value={ativo} onChange={(event) => setAtivo(event.target.value)}></input>
+                            </div>
+                            <div className="bloco">
+                                <p className="textoCadastro">Número de patrimônio:</p>
+                                <input className="inputCadastro" type="int" value={numPatrimonio} onChange={(event) => setNumPatrimonio(event.target.value)}></input>
+                            </div>
+                            <div className="bloco">
+                                <p className="textoCadastro">Descrição:</p>
+                                <input className="inputCadastro" type="text" value={descricao} onChange={(event) => setDescricao(event.target.value)}></input>
+                            </div>
+                        </div>
+                        <div className="areaBotao">
+                            {
+                                isLoading === false &&
+                                <button className="botao" type="submit">Cadastrar</button>
+                            }
+
+                            {
+                                isLoading === true &&
+                                <button className="botao" type="submit" disabled>Carregando...</button>
+                            }
+                        </div>
+                    </form>
+                </div>
+            </section>
+        </div>
+    )
+}
